232e22841d9f0dfe107e529631b346ca1b36db57
sabalyshen <tedoldhead@gmail.com>
2021-07-23 21:59:28 +0800

    avg

--- reply.py
+++ reply.py
@@ -4,11 +4,27 @@ with open('reviews.txt', 'r') as f1:#f == file
     for line in f1:
     	reply.append(line)
     	count += 1
-    	if count % 10000 == 0: 
+    	if count % 100000 == 0: 
     	#'%'是用來求餘數的，1001/1000 餘數=1
     	    print(len(reply))
 
-print(len(reply))
+print('檔案讀取完畢，共有', len(reply), '筆資料')
 print(reply[0])
 print('---------------')
 print(reply[1])
+#資料加總平均，我的答案
+total = 0
+count2 = 0
+with open('reviews.txt', 'r') as f2:#f == file
+    for line2 in f2:
+        count2 += 1
+        total = total + len(line2)
+        if count2 % 1000000 == 0:
+            a = total / len(reply)
+            print(total)
+            print(a)
+#老師答案
+sum_len = 0
+for d in reply:
+	sum_len += len(d) # sum_len = sum_len + len(d)
+print('平均留言長度', sum_len / len(reply))
