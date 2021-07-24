c6083347c6774843b0b05105644a5cc3eeeab680
sabalyshen <tedoldhead@gmail.com>
2021-07-24 20:29:52 +0800

    add_comprehension

--- reply.py
+++ reply.py
@@ -33,13 +33,24 @@ print('平均留言長度', sum_len / len(reply))
 new = []
 for d in reply:#把reply 的每一個東西一個一個叫出來(forloop)
     if len(d) < 100:
-    	new.append(d)
+    	new.append(d) #if你的長度<100我把你裝入new清單
 print('共有', len(new), '筆留言小於100個字母')
 print(new[0])
 
 word = []
 for d in reply:
-    if 'good' in d:
+    if 'good' in d:#if 後面會變是非題
         word.append(d)
 print(len(word))
 print(word[0])
+
+#應用:list comprehension清單快寫法
+""" 整段註解起始
+bad = []
+for b in bad:
+    if 'bad' in b:
+        bad.append(b)
+print('共有', len(b), '筆資料')
+整段註解結束 """ 
+bad = [b for b in reply if 'bad' in b]
+#output = [(number-1運算) for number變數 in reference清單 if number % 2 == 0 篩選條件 ]
